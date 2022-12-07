d62d4d9790d67b0e71b6816f1b72f9a66293e83a
megha.su <megha.su@PIA3298>
2022-12-07 14:08:54 +0530

    Add project files.

---
+++ Anonymous Method[Action]/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Anonymous_Method_Action_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> sumAction = delegate (int firstNumber, int secondNumber)
+            {
+               Console.WriteLine(firstNumber + secondNumber);
+            };
+            Action<int, int> subtractAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber - secondNumber);
+            };
+            Action<int, int> multipliationAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            };
+            Action<int, int> divisionAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            };
+
+            Calculator(sumAction,7, 8);
+            Calculator(subtractAction, 14, 8);
+            Calculator(multipliationAction, 5, 8);
+            Calculator(divisionAction, 16, 8);
+
+        }
+        static void Calculator(Action<int, int> operation, int number1,int number2)
+        {
+            operation(number1, number2);
+        }
+
+    }
+}
