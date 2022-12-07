631722286864064d4e6b0937af15ef6371b516af
Megha-Su <119593325+Megha-Su@users.noreply.github.com>
2022-12-07 14:10:42 +0530

    Add files via upload

---
+++ Anonymous Method[Func].cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Anonymous_method_Func_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> Add = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber + secondNumber;
+            };
+            Func<int, int, int> Subtract = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber - secondNumber;
+            };
+            Func<int, int, int> Multipliation = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber * secondNumber;
+            };
+            Func<int, int, int> Division= delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber / secondNumber;
+            };
+
+            Calculator(Add, 10, 2);
+            Calculator(Subtract, 10, 2);
+            Calculator(Multipliation, 10, 3);
+            Calculator(Division, 10, 5);
+        }
+
+        static void Calculator(Func<int, int, int> operation, int firstNumber, int secondNumber)
+        {
+          Console.WriteLine (operation(firstNumber, secondNumber));
+        }
+
+    }
+}
