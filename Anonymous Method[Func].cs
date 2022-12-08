f3d404dbc07ecf166cc5449196707c409d299131
Megha-Su <119593325+Megha-Su@users.noreply.github.com>
2022-12-08 12:01:32 +0530

    Update Anonymous Method[Func].cs

--- Anonymous Method[Func].cs
+++ Anonymous Method[Func].cs
@@ -7,27 +7,27 @@ namespace Anonymous_method_Func_
         static void Main(string[] args)
         {
 
-            Func<int, int, int> Add = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> add = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber + secondNumber;
             };
-            Func<int, int, int> Subtract = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> subtract = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber - secondNumber;
             };
-            Func<int, int, int> Multipliation = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> multipliation = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber * secondNumber;
             };
-            Func<int, int, int> Division= delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> division= delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber / secondNumber;
             };
 
-            Calculator(Add, 10, 2);
-            Calculator(Subtract, 10, 2);
-            Calculator(Multipliation, 10, 3);
-            Calculator(Division, 10, 5);
+            Calculator(add, 10, 2);
+            Calculator(subtract, 10, 2);
+            Calculator(multipliation, 10, 3);
+            Calculator(division, 10, 5);
         }
 
         static void Calculator(Func<int, int, int> operation, int firstNumber, int secondNumber)
